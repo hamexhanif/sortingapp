af98f7430f9c6c471c267ccb20df9b2d4cab0d08
hamexhanif <hamexhanif@yahoo.com>
2024-12-10 12:28:52 +0100

    add initial file from spring initializer

---
+++ src/main/java/de/sortapp/SortappApplication.java
@@ -0,0 +1,13 @@
+package de.sortapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sortapp/SortappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sortapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
