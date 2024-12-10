4dd6d0a3b326428101ccc27b0600cb327c9e0f8f
hamexhanif <hamexhanif@yahoo.com>
2024-12-10 15:06:29 +0100

    implement bubble sort

--- src/main/java/de/sortapp/service/SortService.java
+++ src/main/java/de/sortapp/service/SortService.java
@@ -1,9 +1,26 @@
 package de.sortapp.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SortService {
     //TODO: implement 3 sorting algorithms (bubble sort, quicksort, merge sort)
+
+    public List<Double> bubbleSort(List<Double> input) {
+        int n = input.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (input.get(j) > input.get(j + 1)) {
+                    // Tausche Elemente
+                    Double temp = input.get(j);
+                    input.set(j, input.get(j + 1));
+                    input.set(j + 1, temp);
+                }
+            }
+        }
+        return input;
+    }
 
 }
